0e63a6db29ee0391f8d85d57151dab37c4a2c7aa
NithishKumarU <60382612+NithishKumarU@users.noreply.github.com>
2021-06-21 19:19:28 +0530

    Update cc.php

--- cc.php
+++ cc.php
@@ -95,7 +95,7 @@ if(isset($_POST['add']))
               <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
                   <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="Home.php">Home</a>
+                    <a class="nav-link active" aria-current="page" href="Index.php">Home</a>
                   </li>
                   <li class="nav-item">
                     <a class="nav-link" href="#recom">Recommended</a>
